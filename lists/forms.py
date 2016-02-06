2dde29d7b7334cad28923589e4f9e45a9de5612d
Sahil Singh <sahilsingh90@gmail.com>
2016-02-06 08:11:03 +0530

    Code formatting

--- lists/forms.py
+++ lists/forms.py
@@ -1,20 +1,25 @@
 from django import forms
 
+
 class UserForm(forms.Form):
-    name = forms.CharField(label="", #To turn off label
-                           max_length=100,
-                           widget=forms.TextInput(attrs={
-                                                    'class':'form-control input-lg',
-                                                    'placeholder':'Your name'
-                                                  })
+    name = forms.CharField(
+        label="",  # To turn off label
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control input-lg',
+                'placeholder': 'Your name'
+            })
     )
 
 
 class ItemForm(forms.Form):
-    item = forms.CharField(label="", #To turn off label
-                           max_length=100,
-                           widget=forms.TextInput(attrs={
-                                                    'class':'form-control input-lg',
-                                                    'placeholder':'Add Item'
-                                                  })
+    item = forms.CharField(
+        label="",  # To turn off label
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control input-lg',
+                'placeholder': 'Add Item'
+            })
     )
